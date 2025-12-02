9d5327ce920f7979ca921b2d2df3dafce6219d0e
Jackblip-max <154344225+Jackblip-max@users.noreply.github.com>
2025-12-02 14:18:08 +0630

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -10,19 +10,50 @@ const User = sequelize.define('User', {
   },
   name: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: {
+      name: 'users_name_unique',
+      msg: 'This username is already taken'
+    },
+    validate: {
+      len: {
+        args: [2, 50],
+        msg: 'Name must be between 2 and 50 characters'
+      },
+      isAlphanumericWithSpaces(value) {
+        if (!/^[a-zA-Z\s]+$/.test(value)) {
+          throw new Error('Name can only contain letters and spaces')
+        }
+      }
+    }
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
+    unique: {
+      name: 'users_email_unique',
+      msg: 'This email is already registered'
+    },
     validate: {
-      isEmail: true
+      isEmail: {
+        msg: 'Please provide a valid email address'
+      }
     }
   },
   phone: {
     type: DataTypes.STRING,
-    allowNull: true
+    allowNull: true,
+    unique: {
+      name: 'users_phone_unique',
+      msg: 'This phone number is already registered'
+    },
+    validate: {
+      isValidPhone(value) {
+        if (value && !/^(\+?95|0?9)\d{7,10}$/.test(value.replace(/\s/g, ''))) {
+          throw new Error('Please provide a valid Myanmar phone number')
+        }
+      }
+    }
   },
   password: {
     type: DataTypes.STRING,
@@ -31,7 +62,10 @@ const User = sequelize.define('User', {
   googleId: {
     type: DataTypes.STRING,
     allowNull: true,
-    unique: true
+    unique: {
+      name: 'users_google_id_unique',
+      msg: 'This Google account is already linked'
+    }
   },
   role: {
     type: DataTypes.ENUM('volunteer', 'organization'),
@@ -52,6 +86,34 @@ const User = sequelize.define('User', {
 }, {
   tableName: 'users',
   timestamps: true,
+  indexes: [
+    {
+      unique: true,
+      fields: ['email'],
+      name: 'users_email_unique'
+    },
+    {
+      unique: true,
+      fields: ['name'],
+      name: 'users_name_unique'
+    },
+    {
+      unique: true,
+      fields: ['phone'],
+      name: 'users_phone_unique',
+      where: {
+        phone: { [sequelize.Sequelize.Op.ne]: null }
+      }
+    },
+    {
+      unique: true,
+      fields: ['googleId'],
+      name: 'users_google_id_unique',
+      where: {
+        googleId: { [sequelize.Sequelize.Op.ne]: null }
+      }
+    }
+  ],
   hooks: {}
 })
 
